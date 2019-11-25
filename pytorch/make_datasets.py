d74469f2a69a54e5a0516f6e19de3b273ae63c01
Kacper Kania <kacp.kania@gmail.com>
2019-11-25 10:19:20 +0100

    Correct to match already implemented scripts

--- pytorch/make_datasets.py
+++ pytorch/make_datasets.py
@@ -33,19 +33,16 @@ def generate_mnist(train_ratio: float) -> t.Dict[str, np.ndarray]:
     )
 
     output = {
-        "train_2d": x_train,
-        "valid_2d": x_valid,
-        "test_2d": x_test,
-        "train_y_true": y_train,
-        "valid_y_true": y_valid,
-        "test_y_true": y_test,
+        "train_2d": x_train.numpy().astype(np.float32) / 255,
+        "valid_2d": x_valid.numpy().astype(np.float32) / 255,
+        "test_2d": x_test.numpy().astype(np.float32) / 255,
     }
 
     return output
 
 
 def generate_mask(input_data: np.ndarray, probability: float) -> np.ndarray:
-    return np.random.binomial(1, probability, size=input_data.shape)
+    return np.random.binomial(1, probability, size=input_data.shape).astype(np.float32)
 
 
 def save_data(
@@ -54,16 +51,28 @@ def save_data(
     output_path = Path("data/") / name
     output_path.mkdir(parents=True, exist_ok=True)
     for noise in tqdm.tqdm(noises):
-        output = {
-            "train_mask": generate_mask(dataset["train_2d"], noise),
-            "valid_mask": generate_mask(dataset["valid_2d"], noise),
-            "test_mask": generate_mask(dataset["test_2d"], noise),
-            **dataset,
-        }
-
         if noise <= 1e-5:
+            output = {
+                "train_x": dataset["train_2d"],
+                "valid_x": dataset["valid_2d"],
+                "test_x": dataset["test_2d"],
+            }
             noise_output_path = output_path / f"{name}.h5"
         else:
+            train_mask = generate_mask(dataset["train_2d"], noise)
+            valid_mask = generate_mask(dataset["valid_2d"], noise)
+            test_mask = generate_mask(dataset["test_2d"], noise)
+            output = {
+                "train_y": dataset["train_2d"] * train_mask,
+                "valid_y": dataset["valid_2d"] * valid_mask,
+                "test_y": dataset["test_2d"] * test_mask,
+                "train_mask": train_mask,
+                "valid_mask": valid_mask,
+                "test_mask": test_mask,
+                "train_y_true": dataset["train_2d"] ,
+                "valid_y_true": dataset["valid_2d"],
+                "test_y_true": dataset["test_2d"],
+            }
             noise_output_path = output_path / f"{name}_{noise:.1f}.h5"
 
         with h5py.File(noise_output_path.as_posix(), "w") as f:
